b5a415f9be9934ee4644785bfda504e35003baae
KS <109473302+dovah-dragon@users.noreply.github.com>
2023-08-28 12:51:03 +0530

    Game Completed

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let inventory = ["stick"];
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3 = document.querySelector('#button3');
+const button4 = document.querySelector('#button4');
 
 //monster stats element
 const monsterStats = document.querySelector('#monsterStats');
@@ -42,7 +43,31 @@ const locations = [
         buttonText : ["Fight Slime", "Fight fanged beast", "Go to town square"],
         buttonFunctions : [fightSlime, fightBeast, goTown],
         text : "You enter the cave. You see some monsters."
-    }
+    },
+    {
+        name : "fight",
+        buttonText : ["Attack", "Dodge", "Run"],
+        buttonFunctions : [attack, dodge, goTown],
+        text : "You are fighting a monster."
+    },
+    {
+        name : "Kill Monster",
+        buttonText : ["Go to town square", "Go to town square", "Go to town square"],
+        buttonFunctions : [goTown, goTown, goTown],
+        text : 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name : "lose",
+        buttonText : ["REPLAY?", "REPLAY?", "REPLAY?"],
+        buttonFunctions : [restart, restart, restart],
+        text : 'You DIE!!!'
+    },
+    {
+        name : "win",
+        buttonText : ["REPLAY?", "REPLAY?", "REPLAY?"],
+        buttonFunctions : [restart, restart, restart],
+        text : 'you defeat the dragon! YOU WIN THE GAME! 👑'
+    },
 ];
 
 const weapons = [
@@ -62,6 +87,24 @@ const weapons = [
         name : "sword",
         power : 100
     }
+];
+
+const monsters = [
+    {
+        name : "slime",
+        level : 2,
+        health : 15
+    },
+    {
+        name : "fanged beast",
+        level : 8,
+        health : 60
+    },
+    {
+        name : "dragon",
+        level : 20,
+        health : 300
+    }
 ]
 
 // initialize buttons
@@ -70,6 +113,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = 'none';
     button1.innerHTML = location.buttonText[0];
     button2.innerHTML = location.buttonText[1];
     button3.innerHTML = location.buttonText[2];
@@ -84,18 +128,11 @@ function update(location) {
 function goStore() {
     update(locations[1]);
 }
-
 function goTown() {
     update(locations[0]);
 }
-
-
-
 function goCave() {
     update(locations[2]);
-}
-function fightDragon() {
-    console.log('fighting dragon');
 }
 
 function buyHealth() {
@@ -103,20 +140,123 @@ function buyHealth() {
     if (gold >= 10) {
         gold = gold - 10;
         health = health + 10;
-        goldText.innerHTML = gold;
-        health.innerHTML = health;
+        goldText.innerText = gold;
+        healthText.innerHTML = health;
     }
     else{
         text.innerText = "You do not have enough gold to buy health";
     }
 }
 function buyWeapon() {
-    if (gold >= 30) {
-        gold = gold - 30;
-        currentWeapon++;
+
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+            gold = gold - 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = 'You have a new weapon.' + newWeapon + ".";
+            inventory.push(newWeapon);
+        }
+        else{
+            text.innerText = "You do not have enough gold to buy weapon";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.addEventListener("click", sellWeapon);
+    }
+}
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold = gold + 15;
         goldText.innerText = gold;
-        let newWeapon = weapons[currentWeapon].name;
-        text.innerText = 'You have a new weapon.' + newWeapon + ".";
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ". | your inventory = [" + inventory + "]" ;
+    } else {
+        text.innerText = "Don't sell your only weapon";
     }
 }
 
+function fightSlime(){
+    fighting = 0;
+    goFight();
+}
+function fightBeast(){
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterHealthText.innerText = monsterHealth;
+    monsterNameText.innerText = monsters[fighting].name;
+}
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks";
+    text.innerText += "You attack it with your " + weapons[currentWeapon].name + ".";
+    health = health - monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) +1;
+    healthText.innerText = health;
+    monsterHealth.innerText = monsterHealth;
+
+    if (health <= 0) {
+        loose();
+    }
+    else if (monsterHealth <= 0) {
+        fighting ===2 ? winGame() : defeatMonster();
+    }
+}
+
+function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function loose() {
+    update(locations[5]);
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function restart() {
+    xp = 0;
+    health = 100;
+    gold = 150;
+    currentWeapon = 0;
+    fighting;
+    monsterHealth;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    healthText.innerText = health;
+    goTown();
+}
+
+function winGame() {
+    update(locations[6]);
+}
+
+//Inventory show
+
+function showInventory() {
+
+    for (let index = 0; index < inventory.length; index++) { 
+        text.innerHTML = inventory;
+    }
+}
+
+button4.onclick = showInventory;
---
+++ scriptTest.js
@@ -0,0 +1,3 @@
+for (let i = 0; i <= 10; i++) {
+    document.write(Math.floor(Math.random() * 10000000)); 
+}
